c3d36ab95b8604c224d7794c7ff7ec8f432ca76a
sam83babak <sam83babak@gmail.com>
2024-04-12 02:18:16 +0330

    init work

---
+++ Dev_Card/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dev_Card.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
